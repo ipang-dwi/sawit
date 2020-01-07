067b2178406ffca51bc3c4febaa25b61be6ed8b3
ipang dwi <admin@firstplato.com>
2020-01-07 21:54:07 +0700

    Update edit-pil.php

--- edit-pil.php
+++ edit-pil.php
@@ -1,17 +1,1 @@
-<?php
-	session_start();
-	if (!isset($_SESSION['login']))
-		header('Location: index.php');
-	include('configdb.php');
-	$pilihan = $_POST['pilihan'];
-	$bobot = $_POST['bobot'];
-	
-	$result = $mysqli->query("UPDATE pilihan SET `pilihan` = '".$pilihan."', `bobot_pilihan` = '".$bobot."' WHERE `id_pilihan` = ".$_GET['id'].";");
-	if(!$result){
-		echo $mysqli->connect_errno." - ".$mysqli->connect_error;
-		exit();
-	}
-	else{
-		header('Location: pilihan.php');
-	}
-?>
+file ini memerlukan donasi, silahkan WA ke no 083833-024200 atau 0856-48587-856
