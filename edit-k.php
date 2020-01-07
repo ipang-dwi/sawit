ecea315490cc6ee1be4fee57e462c4316623b9e3
ipang dwi <admin@firstplato.com>
2020-01-07 21:55:54 +0700

    Update edit-k.php

--- edit-k.php
+++ edit-k.php
@@ -1,16 +1,1 @@
-<?php
-	session_start();
-	if (!isset($_SESSION['login']))
-		header('Location: index.php');
-	include('configdb.php');
-	$penyakit = $_POST['penyakit']; 
-	
-	$result = $mysqli->query("UPDATE penyakit SET `penyakit` = '".$penyakit."' WHERE `id_penyakit` = ".$_GET['id'].";");
-	if(!$result){
-		echo $mysqli->connect_errno." - ".$mysqli->connect_error;
-		exit();
-	}
-	else{
-		header('Location: penyakit.php');
-	}
-?>
+file ini memerlukan donasi, silahkan WA ke no 0856-48587-856
