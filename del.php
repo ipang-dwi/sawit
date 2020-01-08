de370f66e6db186cf6befb32889ad59df710202e
ipang dwi <admin@firstplato.com>
2020-01-08 09:59:46 +0700

    Update del.php

--- del.php
+++ del.php
@@ -1,15 +1,1 @@
-<?php
-	session_start();
-	if (!isset($_SESSION['login']))
-		header('Location: index.php');
-	include('configdb.php');
-	
-	$result = $mysqli->query("delete from alternatif where id_alternatif = ".$_GET['id'].";");
-	if(!$result){
-		echo $mysqli->connect_errno." - ".$mysqli->connect_error;
-		exit();
-	}
-	else{
-		header('Location: alternatif.php');
-	}
-?>
+file ini memerlukan donasi, silahkan WA ke no 0856-48587-856
